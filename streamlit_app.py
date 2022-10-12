2f229bc6c12b0abdb1ede3899806faef5d37de23
Sivasankari <115531035+sivasankari-sankaradayanidhi@users.noreply.github.com>
2022-10-12 21:51:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ try:
       streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
-streamlit.stop()
+#streamlit.stop()
 #import snowflake.connector
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
